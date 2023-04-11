a71631662cba7d0958c35d21d4aa4be69014bc0a
sayeemur <mursalin.eee@gmail.com>
2023-04-11 10:03:16 +0200

    add bootstrap in create-house-form

--- pages/houses/create.jsx
+++ pages/houses/create.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Router from "next/router";
 import { v4 as uuidv4 } from "uuid"
 import axios from "axios";
+import { Container, Row, Form, Image, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CreateHouse = () => {
 	const [inputs, setInputs] = useState({})
@@ -38,7 +40,7 @@ const CreateHouse = () => {
 
 		formData.append('upload_preset', 'yvhpeizk')
 
-		const data = await fetch('https://api.cloudinary.com/v1_1/dxlbetitl/image/upload', {
+		const data = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLIENT_ID}/image/upload`, {
 			method: 'POST',
 			body: formData
 		}).then(r => r.json())
@@ -59,19 +61,24 @@ const CreateHouse = () => {
 		const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address.houseNumber}%20${address.streetName}%20${address.postalCode}%20${address.city}%20${address.country}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
 		await axios.get(url)
 			.then((res) => {
-				console.log("GeoCodingResponse", res)
+				//console.log("GeoCodingResponse", res)
 				const location = res.data.results[0].geometry.location
-				console.log("GeoCodeLocation:", location);
+				//console.log("GeoCodeLocation:", location);
 				setLat(location.lat)
 				setLng(location.lng)
 				setAddress({ ...address, lat: lat, lng: lng })
-				console.log("Address", address)
-				console.log("Lat", lat)
-				console.log("Lan", lng)
+				////console.log("Address", address)
+				//console.log("Lat", lat)
+				//console.log("Lan", lng)
 			})
 			.catch((err) => {
 				console.log(err)
 			})
+		//console.log("newLocation", locationData)
+		//setLat(locationData.lat)
+		//setLng(locationData.lng)
+		//console.log("Lat", lat)
+		//console.log("Lag", lng)
 	}
 
 	const handleChange = async (event) => {
@@ -106,34 +113,292 @@ const CreateHouse = () => {
 	console.log("Upload Data:", uploadData)
 
 	return (
-		<>
-			<div className="createHouse">
-				<h1>Fill up house info</h1>
-			</div>
-			<p>Photo of the house</p>
-			<form method="post" onChange={handleOnChangeImage} onSubmit={handleOnSubmitImage}>
-				<p>
-					<input type="file" name="file" />
-				</p>
+		//<>
+		//	<div className="createHouse">
+		//		<h1>Fill up house info</h1>
+		//	</div>
+		//	<p>Photo of the house</p>
+		//	<form method="post" onChange={handleOnChangeImage} onSubmit={handleOnSubmitImage}>
+		//		<p>
+		//			<input type="file" name="file" />
+		//		</p>
 
-				<img src={imageSrc} />
+		//		<img src={imageSrc} />
+
+		//		{imageSrc && !uploadData && (
+		//			<p>
+		//				<button>Upload photo</button>
+		//			</p>
+		//		)}
+
+		//		{/*{uploadData && (
+		//			<code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+		//		)}*/}
+		//	</form>
+
+		//	<form onSubmit={handleAddressSubmit} action="" method="">
+		//		<p>House address</p>
+		//		<div>
+		//			<label htmlFor="houseNumber">House number:</label>
+		//			<input
+		//				type="number"
+		//				id="houseNumber"
+		//				name="houseNumber"
+		//				value={address.houseNumber || ""}
+		//				onChange={handleAddressChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="streetName">Street name:</label>
+		//			<input
+		//				type="text"
+		//				id="streetName"
+		//				name="streetName"
+		//				value={address.streetName || ""}
+		//				onChange={handleAddressChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="postalCode">Postal code:</label>
+		//			<input
+		//				type="text"
+		//				id="postalCode"
+		//				name="postalCode"
+		//				value={address.postalCode || ""}
+		//				onChange={handleAddressChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="city">City:</label>
+		//			<input
+		//				type="text"
+		//				id="city"
+		//				name="city"
+		//				value={address.city || ""}
+		//				onChange={handleAddressChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="country">Country:</label>
+		//			<input
+		//				type="text"
+		//				id="country"
+		//				name="country"
+		//				value={address.country || ""}
+		//				onChange={handleAddressChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<button>Add Address</button>
+		//	</form>
+
+		//	<form onSubmit={handleSubmit} action="" method="">
+		//		<p>Detail info on the house</p>
+		//		<div>
+		//			<label htmlFor="title">Title:</label>
+		//			<input
+		//				type="text"
+		//				id="title"
+		//				name="title"
+		//				value={inputs.title || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="owner">Owner:</label>
+		//			<input
+		//				type="text"
+		//				id="owner"
+		//				name="owner"
+		//				value={inputs.owner || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="price">Price:</label>
+		//			<input
+		//				type="number"
+		//				id="price"
+		//				name="price"
+		//				value={inputs.price || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="size">Size:</label>
+		//			<input
+		//				type="number"
+		//				id="size"
+		//				name="size"
+		//				value={inputs.size || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="rooms">Rooms:</label>
+		//			<input
+		//				type="number"
+		//				id="rooms"
+		//				name="rooms"
+		//				value={inputs.rooms || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="caution">Caution money:</label>
+		//			<input
+		//				type="number"
+		//				id="caution"
+		//				name="caution"
+		//				value={inputs.caution || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="supermarktDistance">Near by supermarkt distance:</label>
+		//			<input
+		//				type="number"
+		//				id="supermarktDistance"
+		//				name="supermarktDistance"
+		//				value={inputs.supermarktDistance || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="busStationDistance">Near by bus station distance:</label>
+		//			<input
+		//				type="number"
+		//				id="busStationDistance"
+		//				name="busStationDistance"
+		//				value={inputs.busStationDistance || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="maxResident">Number of max resident:</label>
+		//			<input
+		//				type="number"
+		//				id="maxResident"
+		//				name="maxResident"
+		//				value={inputs.maxResident || ""}
+		//				onChange={handleChange}
+		//				required
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="heating">Heating:</label>
+		//			<input
+		//				type="text"
+		//				id="heating"
+		//				name="heating"
+		//				value={inputs.heating || ""}
+		//				onChange={handleChange}
+
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="cooling">Cooling:</label>
+		//			<input
+		//				type="text"
+		//				id="cooling"
+		//				name="cooling"
+		//				value={inputs.cooling || ""}
+		//				onChange={handleChange}
+
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="furnished">Furnished:</label>
+		//			<input
+		//				type="text"
+		//				id="furnished"
+		//				name="furnished"
+		//				value={inputs.furnished || ""}
+		//				onChange={handleChange}
+
+		//			/>
+
+		//		</div>
+		//		<div>
+		//			<label htmlFor="parking">Parking:</label>
+		//			<input
+		//				type="text"
+		//				id="parking"
+		//				name="parking"
+		//				value={inputs.parking || ""}
+		//				onChange={handleChange}
+
+		//			/>
+
+		//		</div>
+
+		//		<div>
+		//			<button type="submit">Create House</button>
+		//		</div>
+		//	</form>
+		//</>
+
+		<Container>
+			<Row>
+				<h1>Fill up house info</h1>
+				<h5>Photo of the house</h5>
+			</Row>
+			<Form method="post" onChange={handleOnChangeImage} onSubmit={handleOnSubmitImage}>
+				<Form.Group>
+
+					<Form.Control type="file" name="file" />
+				</Form.Group>
+
+				<Image src={imageSrc} />
+
+
+
 
 				{imageSrc && !uploadData && (
-					<p>
-						<button>Upload photo</button>
-					</p>
+					<div>
+						<Button type="submit" >Upload photo</Button>
+					</div>
 				)}
 
 				{/*{uploadData && (
-					<code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
-				)}*/}
-			</form>
+				<code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+			)}*/}
+				{uploadData && (<p>Please house address</p>)}
+			</Form>
 
-			<form onSubmit={handleAddressSubmit} action="" method="">
-				<p>House address</p>
-				<div>
-					<label htmlFor="houseNumber">House number:</label>
-					<input
+			<Form onSubmit={handleAddressSubmit} action="" method="">
+				<h5>House address</h5>
+				<Form.Group>
+					<Form.Label>House number</Form.Label>
+					<Form.Control
 						type="number"
 						id="houseNumber"
 						name="houseNumber"
@@ -142,10 +407,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="streetName">Street name:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Street name</Form.Label>
+					<Form.Control
 						type="text"
 						id="streetName"
 						name="streetName"
@@ -154,10 +419,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="postalCode">Postal code:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label >Postal code</Form.Label>
+					<Form.Control
 						type="text"
 						id="postalCode"
 						name="postalCode"
@@ -166,10 +431,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="city">City:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label >City</Form.Label>
+					<Form.Control
 						type="text"
 						id="city"
 						name="city"
@@ -178,10 +443,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="country">Country:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label >Country</Form.Label>
+					<Form.Control
 						type="text"
 						id="country"
 						name="country"
@@ -190,15 +455,16 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<button>Add Address</button>
-			</form>
+				</Form.Group>
+				<Button type="submit">Add Address</Button>
+				{lng && (<p>Now provide the following info</p>)}
+			</Form>
 
-			<form onSubmit={handleSubmit} action="" method="">
-				<p>Detail info on the house</p>
-				<div>
-					<label htmlFor="title">Title:</label>
-					<input
+			<Form onSubmit={handleSubmit} action="" method="">
+				<h5>Detail info on the house</h5>
+				<Form.Group>
+					<Form.Label htmlFor="title">Title</Form.Label>
+					<Form.Control
 						type="text"
 						id="title"
 						name="title"
@@ -207,10 +473,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="owner">Owner:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="owner">Owner</Form.Label>
+					<Form.Control
 						type="text"
 						id="owner"
 						name="owner"
@@ -219,10 +485,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="price">Price:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="price">Price</Form.Label>
+					<Form.Control
 						type="number"
 						id="price"
 						name="price"
@@ -231,10 +497,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="size">Size:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="size">Size</Form.Label>
+					<Form.Control
 						type="number"
 						id="size"
 						name="size"
@@ -243,10 +509,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="rooms">Rooms:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="rooms">Rooms</Form.Label>
+					<Form.Control
 						type="number"
 						id="rooms"
 						name="rooms"
@@ -255,10 +521,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="caution">Caution money:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="caution">Caution money</Form.Label>
+					<Form.Control
 						type="number"
 						id="caution"
 						name="caution"
@@ -267,10 +533,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="supermarktDistance">Near by supermarkt distance:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="supermarktDistance">Near by supermarkt distance</Form.Label>
+					<Form.Control
 						type="number"
 						id="supermarktDistance"
 						name="supermarktDistance"
@@ -279,10 +545,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="busStationDistance">Near by bus station distance:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="busStationDistance">Near by bus station distance</Form.Label>
+					<Form.Control
 						type="number"
 						id="busStationDistance"
 						name="busStationDistance"
@@ -291,10 +557,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="maxResident">Number of max resident:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="maxResident">Number of max resident</Form.Label>
+					<Form.Control
 						type="number"
 						id="maxResident"
 						name="maxResident"
@@ -303,10 +569,10 @@ const CreateHouse = () => {
 						required
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="heating">Heating:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="heating">Heating</Form.Label>
+					<Form.Control
 						type="text"
 						id="heating"
 						name="heating"
@@ -315,10 +581,10 @@ const CreateHouse = () => {
 
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="cooling">Cooling:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="cooling">Cooling</Form.Label>
+					<Form.Control
 						type="text"
 						id="cooling"
 						name="cooling"
@@ -327,10 +593,10 @@ const CreateHouse = () => {
 
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="furnished">Furnished:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="furnished">Furnished</Form.Label>
+					<Form.Control
 						type="text"
 						id="furnished"
 						name="furnished"
@@ -339,10 +605,10 @@ const CreateHouse = () => {
 
 					/>
 
-				</div>
-				<div>
-					<label htmlFor="parking">Parking:</label>
-					<input
+				</Form.Group>
+				<Form.Group>
+					<Form.Label htmlFor="parking">Parking</Form.Label>
+					<Form.Control
 						type="text"
 						id="parking"
 						name="parking"
@@ -351,13 +617,13 @@ const CreateHouse = () => {
 
 					/>
 
-				</div>
+				</Form.Group>
 
 				<div>
-					<button type="submit">Create House</button>
+					<Button type="submit">Create House</Button>
 				</div>
-			</form>
-		</>
+			</Form>
+		</Container>
 	);
 };
 
